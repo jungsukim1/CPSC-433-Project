074a974bb8924ef48add3296e9a2f4bf4ce6f58c
Poporuo <anekin03@gmail.com>
2024-12-05 18:01:23 -0700

    changed delete logic

--- Delete.py
+++ Delete.py
@@ -2,18 +2,20 @@ from Slots import GameSlot, PracticeSlot
 from Schedule import Schedule
 import random
 
-def Delete(schedule):
-    amount_to_delete = random.randint(1,schedule.totalGames + schedule.totalPractices - 1)
+def Delete(fact):
+    # amount_to_delete = random.randint(1,schedule.totalGames + schedule.totalPractices - 1)
     
-    for i in range(amount_to_delete):
-        choice = random.randint(0,1)
+    # for i in range(amount_to_delete):
+    #     choice = random.randint(0,1)
         
-        if choice == 0:
-            rand_index = random.randint(0,schedule.totalGames)
-            schedule.gameslots[rand_index].removeGame()
+    #     if choice == 0:
+    #         rand_index = random.randint(0,schedule.totalGames)
+    #         schedule.gameslots[rand_index].removeGame()
             
-        else:
-            rand_index = random.randint(0,schedule.totalPractices)
-            schedule.practiceslots[rand_index].removePractice()
+    #     else:
+    #         rand_index = random.randint(0,schedule.totalPractices)
+    #         schedule.practiceslots[rand_index].removePractice()
     
+    del fact[0]
+    return fact
     
